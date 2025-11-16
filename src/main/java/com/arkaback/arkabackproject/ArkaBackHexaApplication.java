0e8ed6c5adc55f7a51d984d811fb4cbedf3bc993
Johan Diaz <johandiaz@Mac.home>
2025-11-16 16:15:01 +0100

    primer commit project arka

---
+++ src/main/java/com/arkaback/arkabackproject/ArkaBackHexaApplication.java
@@ -0,0 +1,13 @@
+package com.arkaback.arkabackproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkaBackHexaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArkaBackHexaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arkaback/arkabackproject/ArkaBackHexaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arkaback.arkabackproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkaBackHexaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
